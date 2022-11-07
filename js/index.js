b9661c9f1c9768eafac8834f5228b41a7f4d4729
prankland3 <thibaut.verbeke2005@hotmail.com>
2022-11-07 18:14:36 +0100

    innit

--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-let firstCard = 10
-let secondCard = 7
-let sum = firstCard + secondCard +4
-let hasBlackJack = undefined
-let isAlive = undefined
 
-let message = ""
+    let firstCard = 10
+    let secondCard = 7
+    let sum = firstCard + secondCard +4
+    let hasBlackJack = undefined
+    let isAlive = undefined
+    let message = ""
 
-if (sum <= 20){
+function startGame() {
+ if (sum <= 20){
     message = "Do you want to draw a new card? 🙂"
-
     hasBlackJack = false 
     isAlive = true
 }else if (sum ===21){
     message ="Wohoo! You've got Blackjack! 🥳"
-   
     hasBlackJack = true
     isAlive = true
 }else{
-     message = "You're out of the game! 😭"
+    message = "You're out of the game! 😭"
     hasBlackJack = false 
     isAlive = false
 }
@@ -25,3 +24,4 @@ if (sum <= 20){
 //cash out
 
 console.log(message)
+}
